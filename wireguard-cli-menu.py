269c69409fdadb47c21a4e77ca77b13fbfc7f0ee
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-02-02 16:03:35 -0500

    Update wireguard-cli-menu.py

--- wireguard-cli-menu.py
+++ wireguard-cli-menu.py
@@ -14,26 +14,35 @@ while True:
     
     #main menu prompt
     menu_clear()
-    options = ["Start Wireguard", "Stop Wireguard", "Exit"]
-    terminal_menu = TerminalMenu(options)
+    menu_title = ("Main menu for wireguard CLI\n")
+    options = (["Start Wireguard", "Stop Wireguard", "Exit"])
+    main_menu_cursor_style = ("fg_gray", "bold")
+    #main menu options
+    terminal_menu = TerminalMenu(
+    menu_entries=options,
+    title=menu_title,
+    menu_cursor="-",
+    menu_cursor_style=main_menu_cursor_style
+    )
+    
     menu_entry_index = terminal_menu.show()
-    menu = ({options[menu_entry_index]})
+    menu = (options[menu_entry_index])
 
     #runs wireguard if user selects to start
-    if menu == {"Start Wireguard"}:
+    if menu == ("Start Wireguard"):
         menu_clear()
         subprocess.run(wg_up)
         input("wireguard is running! Press enter to the main menu\n")
         continue
     
     #stops wireguard if user selects to top
-    elif menu == {"Stop Wireguard"}:
+    elif menu == ("Stop Wireguard"):
         menu_clear()
         subprocess.run(wg_down)
         input("wireguard is not running! Press enter to the main menu\n")
         continue
     
     #exits program
-    elif menu == {"Exit"}:
+    elif menu == ("Exit"):
         print("have a good one!")
         break
